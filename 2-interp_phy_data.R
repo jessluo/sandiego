20f506815a78156293ed033b6bfa4a7a72e6cbe1
Jean-Olivier Irisson <irisson@normalesup.org>
2013-02-07 01:49:48 +0100

    Review comments and write result as text

--- 2-interp_phy_data.R
+++ 2-interp_phy_data.R
@@ -11,12 +11,17 @@ library("ggplot2")
 # setup R to keep decimal seconds in the times
 options("digits.secs"=3)
 
-bio <- read.csv("data/bio.csv", colClasses=c(dateTime="POSIXct"), stringsAsFactors=FALSE)
-phy <- read.csv("data/phy.csv", colClasses=c(dateTime="POSIXct"), stringsAsFactors=FALSE)
+# read data
+phy <- read.csv("data/phy.csv", stringsAsFactors=FALSE)
+phy$dateTime <- as.POSIXct(phy$dateTime, tz="GMT")
+# NB: make sure time is set in GMT (even if it wasn't) to avoid dealing with tz afterwards
+bio <- read.csv("data/bio.csv", stringsAsFactors=FALSE)
+bio$dateTime <- as.POSIXct(bio$dateTime, tz="GMT")
+
 
 ##{ Interpolate physical data ---------------------------------------------
-
 # interpolate physical parameters for each biological sample
+
 # unique biological sample times
 times <- sort(unique(bio$dateTime))
 
@@ -37,3 +42,9 @@ interp <- cbind(interp, phy[closestIndex, c("transect", "cast", "down.up")])
 bio <- join(bio, interp)
 
 # }
+
+# convert counts into presence
+bio$presence <- bio$count > 0
+
+# write it as text
+write.csv(bio, file="data/all_presence_only.csv", row.names=FALSE)
