c2a048f6ff4607f1da0c436b233777ddbf975a78
JessLuo <jessica.luo@rsmas.miami.edu>
2013-02-07 18:42:18 +0100

    added in more single variate regression trees and multivariate regression trees

--- analysis-regression_trees.R
+++ analysis-regression_trees.R
@@ -44,12 +44,27 @@ ggplot(dg[dg$concentration > 0,]) + geom_histogram(aes(x=log(concentration))) + 
 
 # }
 
+##{ Regression trees -------------------------------------------------------
+
 
 ##{ Per group trees -------------------------------------------------------
 
 m <- mvpart(concentration ~ temp + salinity + fluoro + oxygen, data=dg[dg$group2=="sol_small",], xv=("none"))
 m <- mvpart(concentration ~ depth + long, data=dg[dg$group2=="sol_small",], xv=("min"), xval=5, xvmult=100)
 m <- mvpart(concentration ~ temp + salinity + fluoro + oxygen, data=dg[dg$group2=="sol_small",], xv=("min"), xval=5, xvmult=100)
+
+# apps are not very interesting
+m <- mvpart(concentration ~ depth + long + temp + salinity + fluoro + oxygen, data=dg[dg$group2=="appendicularians",], xv=("min"), xval=5, xvmult=100)
+
+# hydromedusae
+m <- mvpart(concentration ~ temp + salinity + fluoro + oxygen, data=dg[dg$group2=="Hydromedusae",], xv=("1se"), xval=5, xvmult=100)
+
+# siphonophores
+m <- mvpart(concentration ~ temp + salinity + fluoro + oxygen, data=dg[dg$group2=="Siphonophores",], xv=("min"), xval=5, xvmult=100)
+
+# ctenophores - cannot explain ctenophores with this method because they are not numerous enough. can only use logistic regression
+m <- mvpart(concentration ~ temp + salinity + fluoro + oxygen, data=dg[dg$group2=="Ctenophores",], xv=("min"), xval=5, xvmult=100)
+
 
 # }
 
@@ -64,5 +79,30 @@ dSol <- join(dSol, unique(d[,c("dateTimeB", vars)]))
 m <- mvpart(sol_small ~ temp + salinity + fluoro + oxygen + sol_large, data=dSol, xv=("1se"), xval=5, xvmult=100)
 m <- mvpart(sol_small ~ depth + long + sol_large, data=dSol, xv=("1se"), xval=5, xvmult=100)
 
+dgC <- dcast (dg, dateTimeB + transect + cast + down.up + depth + long + temp + salinity + fluoro + oxygen ~ group2, value.var="concentration")
+
+# multivariate regression tree with all taxa
+m <- mvpart(data.matrix(dgC[,names(dgC) %in% c("appendicularians", "Ctenophores", "doliolids", "Hydromedusae", "Siphonophores", "sol_large", "sol_small")]) ~ temp + salinity + fluoro + oxygen, data=dgC, xv=("min"), xval=5, xvmult=100)
+# basically, when you look at the multivariate regression tree for everything, it is 27% explained by temperature (which is probably just a proxy for depth) and then nothing else
+
+# multivariate regression tree without the apps
+m <- mvpart(data.matrix(dgC[,names(dgC) %in% c("Ctenophores", "doliolids", "Hydromedusae", "Siphonophores", "sol_large", "sol_small")]) ~ temp + salinity + fluoro + oxygen, data=dgC, xv=("min"), xval=5, xvmult=100)
+# much better. though weird/repeating color scale. and difficult to see as it is overlaying one of the branches
+
+# multivariate regression tree with just the jellies and the ctenophores
+m <- mvpart(data.matrix(dgC[,names(dgC) %in% c("Ctenophores", "Hydromedusae", "Siphonophores", "sol_large", "sol_small")]) ~ temp + salinity + fluoro + oxygen, data=dgC, xv=("min"), xval=5, xvmult=100)
+
+# multivariate regression tree with just the hydrozoans
+m <- mvpart(data.matrix(dgC[,names(dgC) %in% c("Hydromedusae", "Siphonophores", "sol_large", "sol_small")]) ~ temp + salinity + fluoro + oxygen, data=dgC, xv=("min"), xval=5, xvmult=100)
+# the latter three are about the same
+# think about the interpretation for these plots ...
+
+
 # }
 
+# }
+
+# TODO:
+# CA with species
+# CCA or RDA with species ~ environment
+# logistic regression on P/A data
