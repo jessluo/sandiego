4a834a4ab08b3034f32a716495735b1bc63ff0db
JessLuo <jessica.luo@rsmas.miami.edu>
2013-02-08 19:12:06 +0100

    added a comment about not being able to interpolate salinity

--- prepare-interp_physical.R
+++ prepare-interp_physical.R
@@ -136,5 +136,6 @@ ti$dist <- ti$dist * f
 
 ggplot(ti) + geom_tile(aes(x=dist, y=-depth, fill=temp)) + geom_contour(aes(x=dist, y=-depth, z=temp), colour="white", size=0.5, alpha=0.5, breaks=c(10, 15)) + facet_grid(transect~.)
 
+# tried to interpolate salinity but it is returning NAs
 # }
 
