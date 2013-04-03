2a0baeb3709262bdc929484ee499c1a76823f4c2
JessLuo <jessica.luo@rsmas.miami.edu>
2013-04-03 15:43:15 -0400

    additional violin and density plots, as well as some narcomedusae/trachymedusae/predator-prey plots

--- plotting-densities.R
+++ plotting-densities.R
@@ -79,5 +79,66 @@ ggplot(data=df[which(df$concentration > 0),]) + geom_point(mapping=aes(x=-depth,
 
 # ggplot(data=df[which(df$concentration > 0),]) + geom_point(mapping=aes(x=concentration, y=-depth), stat="identity") + labs(title="Appendicularian densities depth profile", x="Density", y="Depth") + ylim(-135, 0) + scale_x_continuous(guide=list(guide_axis(), guide_axis(position="top", trans = function(x) x * 2)))
 
+# everything together
+
+# identify explanatory variables of interest
+locVars <- c("depth", "long")
+hydroVars <- c("temp", "salinity", "fluoro", "oxygen", "swRho")
+vars <- c(locVars, hydroVars)
+# define new groups for the analysis
+d$group2 <- d$group
+# d$group2[d$group == "Solmaris"] <- d$taxon[d$group == "Solmaris"]
+d$group2[d$group == "Tunicates"] <- d$taxon[d$group == "Tunicates"]
+
+# compute total concentration per group
+dg <- ddply(d, ~transect + cast + down.up + front + dateTimeB + group2, function(x) {
+  tot <- sum(x$concentration)
+  timeavg <- mean(x$dateTime)
+  return(data.frame(x[1,vars], concentration=tot, dateTime=timeavg))
+}, .parallel=TRUE)
+
+
+# casting data into wide format with group 2
+dgC <- dcast (dg, dateTimeB + dateTime + transect + cast + down.up + front + depth + long + temp + salinity + swRho + fluoro + oxygen ~ group2, value.var="concentration")
+
+
+# violin plots
+ggplot(d[d$concentration>0,]) + geom_violin(aes(x=group2, y=-depth, weight=concentration, colour=group2), alpha=0.4) + facet_grid(transect~front)
+
+ggplot(d[d$concentration>0,]) + geom_violin(aes(x=front, y=-depth, weight=concentration, colour=group2), alpha=0.4) + facet_grid(transect~group2) + labs(colour="Taxon")
+
+
+ggplot(d) + geom_path(aes(x=-depth, y=concentration, colour=group2), alpha=0.5) + coord_flip() + labs(y="Density", x="Depth", title="Depth distribution", colour="Taxon")
+
+ggplot(d[d$taxon != "appendicularians",]) + geom_density(aes(x=-depth, weight=concentration, colour=group2), alpha=0.5) + facet_grid(transect~.) + coord_flip() + labs(y="Density", x="Depth", title="Depth distribution", colour="Taxon")
+
+dd <- data.frame(x=1, y=sort(rnorm(100)), z=1:100)
+ggplot(dd) + geom_violin(aes(x=x, y=y))
+ggplot(dd) + geom_violin(aes(x=x, y=y, weight=z))
+
+
+
+ggplot(d[d$concentration>0 & d$group != "Tunicates",]) + geom_point(aes(x=long, y=-depth, size=concentration, colour=group), alpha=0.4) + facet_grid(transect~.)
+
+
+ggplot(d[d$concentration>0,]) + geom_bar(aes(x=-depth, fill=group2), stat="bin", position="stack") + coord_flip() + facet_grid(transect~.)
+
+# why doesn't this work?
+# need to sum the abundances
+ggplot(d[d$concentration>0,]) + geom_bar(aes(x=-depth, y=concentration, fill=group), stat="identity", position="stack") + coord_flip() + facet_grid(transect~.)
+
+# pred / prey
+narco <- c("h10_Pegantha", "h3_Cunina", "h6_Solmundella", "h7_Pegantha", "r4_Aegina", "sol_large", "sol_small")
+trachy <- c("h11_Haliscera", "h2_Haliscera", "h5_Liriope", "h5b", "h7_Rhopalonema", "h9_Aglaura", "h9_Arctapodema")
+pred <- c(narco, "Beroida")
+
+ggplot(d[d$taxon %in% narco,]) + geom_point(aes(x=long, y=-depth, size=concentration, colour=concentration>0), alpha=0.7) + facet_grid(transect~taxon) + scale_colour_manual(values=c("grey70", "black")) + scale_area(range=c(1,10))
+
+ggplot(d[d$taxon %in% trachy,]) + geom_point(aes(x=long, y=-depth, size=concentration, colour=concentration>0), alpha=0.7) + facet_grid(transect~taxon) + scale_colour_manual(values=c("grey70", "black")) + scale_area(range=c(1,10))
+
+ggplot() + geom_point(aes(x=long, y=-depth, size=concentration), colour="pink", alpha=0.7, data=d[d$group=="Ctenophores" & d$taxon !="Beroida" & d$concentration > 0,]) + facet_grid(transect~.) + scale_area(range=c(1,10))
+
+ggplot() + geom_point(aes(x=long, y=-depth, size=concentration), colour="pink", alpha=0.7, data=d[d$group=="Ctenophores" & d$taxon !="Beroida" & d$concentration > 0,]) + geom_point(aes(x=long, y=-depth, size=concentration), colour="black", alpha=0.7, data=d[d$group=="Ctenophores" & d$taxon=="Beroida" & d$concentration >0,]) + facet_grid(transect~.) + scale_area(range=c(1,10))
 # }
+
 
